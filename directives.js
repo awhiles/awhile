4d5804aa3e9d64d48bc1e9550935dcacf2f075a8
liangjiang <xuan03121994@163.com>
2017-06-28 11:14:21 +0800

    banner edit

--- directives.js
+++ directives.js
@@ -20,10 +20,39 @@ angular.module('app')
             replace: true,
             templateUrl:'template/banner.html',
             scope: {
+                pos: '=',
                 bannerData: '='
             },
             link:function (scope,el,attr) {
+                var pos = scope.pos,
+                    E = $(el).find('li'),
+                    Rbtn = $(el).find('.btn-right'),
+                    Lbtn = $(el).find('.btn-left');
+                // confirm position
+                $.each(E,function(index,val){
+                    E.eq(index).css('zIndex',pos[index].z).stop(true,true).animate(pos[index],0)
+                });
+                // right direction
+                Rbtn.click(function(){
+                    motionSwitch(true)
+                });
+                // left direction
+                Lbtn.click(function(){
+                    motionSwitch(false)
+                });
+                // motion switchover
+                function motionSwitch(F){
+                    if(F){
+                        pos.push(pos.shift());
 
+                    }else{
+                        pos.unshift(pos.pop());
+
+                    }
+                    $.each(E,function(index,val){
+                        E.eq(index).css('zIndex',pos[index].z).stop(true,true).animate(pos[index],250)
+                    })
+                }
             }
         }
     });
--- js/controller/read.js
+++ js/controller/read.js
@@ -1,4 +1,11 @@
 angular.module('app')
     .controller('readCtrl',['$scope',function($scope){
-        
+        $scope.readPos = [
+            {left:150,top:0,width:640,height:375,z:8},
+            {left:0,top:33,width:493,height:289,z:6},
+            {left:300,top:53,width:410,height:240,z:4},
+            {left:320,top:73,width:341,height:200,z:2},
+            {left:50,top:93,width:256,height:150,z:0},
+            {left:467,top:33,width:493,height:289,z:6}
+        ]
     }]);
